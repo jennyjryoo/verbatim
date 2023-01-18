9816a907662b42db22afd728a0774a6108e7e5fd
jennyryoo <jennyjryoo@gmail.com>
2023-01-18 12:47:31 -0800

    adding spring boot project

---
+++ Verbatim-Server/src/main/java/com/verbatim/VerbatimServer/VerbatimServerApplication.java
@@ -0,0 +1,13 @@
+package com.verbatim.VerbatimServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbatimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbatimServerApplication.class, args);
+	}
+
+}
---
+++ Verbatim-Server/src/test/java/com/verbatim/VerbatimServer/VerbatimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verbatim.VerbatimServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbatimServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
